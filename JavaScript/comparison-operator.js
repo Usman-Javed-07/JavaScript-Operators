df49f3e9d833f1d8c1655feafd9d720b876b24cc
Usman Javed <usmanjaved0816@gmail.com>
2024-09-28 17:06:13 +0500

    assignments operators

---
+++ JavaScript/comparison-operator.js
@@ -0,0 +1,17 @@
+x = 1;
+
+
+// relational operators
+console.log(x>=1); // true
+
+console.log(x<=1); //true
+
+console.log(x>1); // false
+
+console.log (x<1); // false
+
+// equality operators 
+
+console.log(x===1); //ture
+console.log(x !==1); //false
+
