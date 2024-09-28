eae789ed01a07272cdb65ab0989bbeb864ecca62
Usman Javed <usmanjaved0816@gmail.com>
2024-09-28 16:20:04 +0500

    arithmetic operators

---
+++ JavaScript/arithimatic-operators.js
@@ -0,0 +1,43 @@
+let x = 7;
+let y = 3;
+
+// arithmetic Operators 
+
+// Addition 
+
+console.log(x+y);
+
+// subtraction 
+
+console.log(x-y);
+
+// multiplication
+
+console.log(x*y);
+
+// devision 
+
+console.log(x/y);
+
+// remender of devision 
+
+console.log(x%y);
+
+// exponension 
+
+console.log(x**y);
+
+// increment  (++)
+
+// console.log (++x); // = 8
+
+console.log (x++); // first print x a and than increment 1 so this time it print 7 
+console.log (x); // now it is 8 
+
+
+// decrement Operator 
+
+console.log (--x); // x = 7 
+
+console.log (x--); // x = 7
+console.log(x); // x = 6
