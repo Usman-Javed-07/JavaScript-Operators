0dd57f430d319283cb397ed92dc9aac5f9db2b09
Usman Javed <usmanjaved0816@gmail.com>
2024-09-28 18:03:22 +0500

    logical operators

---
+++ JavaScript/logical-operators.js
@@ -0,0 +1,24 @@
+// logical And operator  (&&)
+// It return true when both the operands are true
+// let HignIncome = true;
+// let GoodCreditScore = true;
+// let eligiableForLone = HignIncome && GoodCreditScore;
+
+// console.log(eligiableForLone);
+
+
+
+
+// logical Or Operator (||) 
+// It return true when any one of the operands are true
+let HignIncome = false;
+let GoodCreditScore = false;
+let eligiableForLone = HignIncome || GoodCreditScore;
+
+console.log( 'eligiable',eligiableForLone);
+
+// not operator
+
+let applicationRefuse = !eligiableForLone;
+
+console.log('application refused' , applicationRefuse);
