d32305b320da50adcec1931bfc3d2b6f4563ece7
Usman Javed <usmanjaved0816@gmail.com>
2024-09-28 16:41:46 +0500

    assignment operators

---
+++ JavaScript/assignment-operators.js
@@ -0,0 +1,19 @@
+let x = 5; 
+
+// assignment addition operators
+
+x += 5 ;
+
+console.log (x);
+
+// assignment subtraction operators
+
+x -= 3; 
+
+console.log(x);
+
+
+// assignment multiplication operators
+
+x *= 2 ;
+console.log (x); 
