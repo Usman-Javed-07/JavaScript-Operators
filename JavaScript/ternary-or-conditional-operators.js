ec4a39aa3f8f47b5c77205988291305bf3cf67c7
Usman Javed <usmanjaved0816@gmail.com>
2024-09-28 17:44:10 +0500

    ternary operators

---
+++ JavaScript/ternary-or-conditional-operators.js
@@ -0,0 +1,8 @@
+// if a custmor has more than 100 points , 
+// the custmor is a gold custmor otherwise, 
+// it is a silver custmor 
+
+let points = 100; 
+let type = points > 110 ? 'gold' : 'silver';
+
+console.log(type);
