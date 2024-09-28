30a1d05018dff79144ac9f37905fb80dd2ccaf64
Usman Javed <usmanjaved0816@gmail.com>
2024-09-28 19:12:18 +0500

    exercise swaping variabeles

---
+++ JavaScript/exercise-swaping-variables.js
@@ -0,0 +1,18 @@
+
+
+//  swaping variables 
+
+let a  = 'red';
+let b  =  'blue';
+
+
+let temp = a ;
+// first method 
+// a : a = b;
+// b : b = temp;
+
+// 2nd mehthod 
+a = b; 
+b = temp; 
+console.log(a);
+console.log(b);
