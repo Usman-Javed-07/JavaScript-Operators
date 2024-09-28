744a587634c52116243e58099dbaf454c76f06ce
Usman Javed <usmanjaved0816@gmail.com>
2024-09-28 17:19:52 +0500

    strict and lose equality operators

---
+++ JavaScript/equality-operators.js
@@ -0,0 +1,10 @@
+
+// strict Equality operators
+
+console.log( 1 === 1); // true 
+console.log('1' === 1); // false
+
+// lose equality operators 
+
+console.log ('1' ==1); //true
+console.log (true == 1);  // true 
